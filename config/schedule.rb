df48acb1f39a534034e11097513e1f0debf61479
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-07 05:29:42 +0100

    Fix whatever configuration, task is called broadcast.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every :hour do
-  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && bundle exec bin/collector run"
+  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && bundle exec bin/collector broadcast"
 end
